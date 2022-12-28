78b5a15eb06bc7175654963274c03a2caa1b58c4
Dev-Comfie <devcomfiepro@gmail.com>
2022-12-28 13:51:57 +0100

    added changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,14 @@ import Services from "./components/Services/Services";
 import Cta from "./components/Cta/Cta";
 import Testimonials from "./components/Testimonials/Testimonials";
 import Footer from "./components/Footer/Footer";
+import Hero from "./components/Hero/Hero";
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
+      <Hero/>
       <Destinations />
       <About />
       <Services />
